b09127db6025ac4f4a0cdec46ba15ff34ade357a
maligujjar2801 <mhamza19851@gmail.com>
2026-05-07 11:58:51 -0700

    Create Report card printer.py
    This is my first workshop in freecodecamp python basics, I've changed the hardcoded values to user input methods.

---
+++ Report card printer.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")#name="Ali"
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age =int(input("Enter your age:"))#age=17
+print(age, type(age))
+
+score =float(input("Enter your score:"))#score=86.4
+print(isinstance(score, float))
+
